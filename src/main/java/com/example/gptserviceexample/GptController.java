849c92af00cdb9b48abbb28224f83b8c9bb1cad8
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2023-09-14 11:17:32 +0300

    switched to anaconda and latest nano gpt

--- src/main/java/com/example/gptserviceexample/GptController.java
+++ src/main/java/com/example/gptserviceexample/GptController.java
@@ -15,18 +15,7 @@ public class GptController {
     GptController(){
         MainInterpreter.setSharedModulesArgv("",
                 "config/train_shakespeare_char.py",
-                "--log_interval=1",
-                "--n_layer=4",
-                "--n_head=4",
-                "--n_embd=128",
-                "--device=cpu",
-                "--compile=False",
-                "--eval_iters=20",
-                "--block_size=64",
-                "--batch_size=12",
-                "--max_iters=2000",
-                "--lr_decay_iters=2000",
-                "--dropout=0.0");
+                "--compile=False");
         try {
             relativeGPTmasterPath = new File("build/python/nanoGPT-master").getCanonicalPath().replace('\\', '/');
         } catch (IOException e) {
